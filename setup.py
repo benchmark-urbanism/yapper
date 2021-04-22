ed8e468aa14f931a8e314f538fb7ef619805748f
Gareth Simons <garethsimons@me.com>
2021-04-22 15:16:55 +0100

    reworks ci

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yapper',
-    version='0.0.1',
+    version='0.0.2a1',
     packages=find_packages(),
     scripts=['yapper/__init__.py'],
     entry_points={
